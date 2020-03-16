da1a5a7a08cf99aa4ecb451f0ae8169e2b717a06
Colin Brayton <gringolalia@gmail.com>
2020-03-16 11:17:16 -0300

    deploy

---
+++ precache-manifest.95c0dc2808d2519875af8ee8a753ddc1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0626eba6afc5bd35ea52",
+    "url": "/rocket-to-venus/css/app.791b4f57.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/rocket-to-venus/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d3b09b615c980d50f986030359d565a7",
+    "url": "/rocket-to-venus/index.html"
+  },
+  {
+    "revision": "8f2c5e57fc2f669d7cee",
+    "url": "/rocket-to-venus/js/about.91a6a1f6.js"
+  },
+  {
+    "revision": "0626eba6afc5bd35ea52",
+    "url": "/rocket-to-venus/js/app.c66cbfbb.js"
+  },
+  {
+    "revision": "342bd5ba24e53759e7f8",
+    "url": "/rocket-to-venus/js/chunk-vendors.5aee0011.js"
+  },
+  {
+    "revision": "0aa50aeffd9606e3f503966f15fbeb0b",
+    "url": "/rocket-to-venus/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rocket-to-venus/robots.txt"
+  },
+  {
+    "revision": "08e400231efd676594030084268546f1",
+    "url": "/rocket-to-venus/styles/more.less"
+  },
+  {
+    "revision": "0dd870f875d7bb28e107198f530b78af",
+    "url": "/rocket-to-venus/styles/variant.less"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rocket-to-venus/precache-manifest.95c0dc2808d2519875af8ee8a753ddc1.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "rocket-to-venus"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
